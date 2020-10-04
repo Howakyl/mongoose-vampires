43636fa0e74201f3788c4a866d8e305862b87c8e
Howakyl96 <howakyl@gmail.com>
2020-10-04 15:44:04 -0700

    commit 4 - selected vampires

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -4,6 +4,7 @@ const connectionString = 'mongodb://localhost:27017/vamptest';
 // 2. Require your Model
 // const db = require('./models');
 const Vampire = require('./models/vampire');
+const { filter } = require('./populateVampires');
 const populateVampires = require('./populateVampires');
 // 3. Require your extra data source
 const vampData = require('./populateVampires');
@@ -140,19 +141,57 @@ mongoose.connection.on('error', (error) => {
 // });
 
 //5) have greater than 150 AND fewer than 500 victims
-Vampire.find({victims: {$gt: 150 , $lt: 500}} , (error, filteredVampires) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(filteredVampires);
-    }
-    process.exit();
-});
+// Vampire.find({victims: {$gt: 150 , $lt: 500}} , (error, filteredVampires) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log(filteredVampires);
+//     }
+//     process.exit();
+// });
 
 
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
+//1) have a title property
+// Vampire.find({title: {$exists: true}} , (error,titleVampires) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log(titleVampires);
+//     }
+//     process.exit();
+// });
 
+//2) do not have a victims property
+// Vampire.find({victims: {$exists: false}} , (error,noVictims) => {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log(noVictims)
+//     }
+//     process.exit();
+// });
+
+//3) have a title AND no victims
+// Vampire.find({victims: {$exists: false} , title: {$exists: true}} , (error, filteredVampires) => {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log(filteredVampires)
+//     }
+//     process.exit();
+// });
+
+//4) have victims AND the victims they have are greater than 1000
+Vampire.find({victims: {$exists: true} , victims: {$gt: 1000}} , (error, filteredVampires) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(filteredVampires)
+    }
+    process.exit();
+});
 /////////////////////////////////////////////////
 // ### Select with OR
 
